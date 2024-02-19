95d713eedbc0876f5acfaa84c8894bab46786f2e
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-19 16:26:06 +0530

    feat: added the starter root file

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const database = require("./db");
+const InventoryRouter = require("./routers/Inventory.router");
+const SaleRouter = require("./routers/Sale.router");
+
+const app = express();
+
+database();
+
+app.use(express.json());
+app.use(cors());
+
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong", err });
+});
+
+app.get("/", (req, res) => {
+  res.send("Inventory Management Backend");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Inventory Management Backend server started");
+});
